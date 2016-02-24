dd798c58183f06d890f6f6384a4c3beb77744f08
ArturGodunov <artur-godunov@mail.ru>
2016-02-24 16:53:52 +0300

    add popup small,popup big

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,9 @@ module.exports = function(grunt) {
 					'dist/index4.html': 'src/jade/index4.jade',
 					'dist/index5.html': 'src/jade/index5.jade',
 					'dist/index6.html': 'src/jade/index6.jade',
-					'dist/popup1.html': 'src/jade/includes/blocks/popup1.jade'
+					'dist/popup1.html': 'src/jade/includes/blocks/popup1.jade',
+					'dist/popup2.html': 'src/jade/includes/blocks/popup2.jade',
+					'dist/popup3.html': 'src/jade/includes/blocks/popup3.jade'
 				}
 			}
 		},
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -3,19 +3,35 @@ $(document).ready(function () {
     /**
      * Popup Welcom
      * */
-    //$.colorbox({
-    //    href:'popup1.html',
-    //    opacity: 1,
-    //    overlayClose: false,
-    //    escKey: false,
-    //    arrowKey: false,
-    //    closeButton: false,
-    //    onComplete: function(){
-    //        setTimeout(function() {
-    //            $.colorbox.close();
-    //        }, 1500)
-    //    }
-    //});
+    $.colorbox({
+        href:'popup1.html',
+        opacity: 1,
+        arrowKey: false,
+        closeButton: false,
+        onComplete: function(){
+            setTimeout(function() {
+                $.colorbox.close();
+            }, 1500)
+        }
+    });
+
+    /**
+     * Popup Transaction Details
+     * */
+    $('[data-popup-small]').colorbox({
+        href:'popup2.html',
+        opacity: 1,
+        arrowKey: false
+    });
+
+    /**
+     * Popup Processor Details
+     * */
+    $('[data-popup-big]').colorbox({
+        href:'popup3.html',
+        opacity: 1,
+        arrowKey: false
+    });
 
     /**
      * Custom select
@@ -58,23 +74,53 @@ $(document).ready(function () {
    });
 
     /**
-     * Tooltip
+     * Tooltip string
      * */
-    $('[data-string-tooltip]').tooltip({
+    $('[data-tooltip-string]').tooltip({
         position: {
             my: 'left bottom-15',
-            at: 'left top',
-            using: function(position, feedback) {
+            at: 'left+10% top',
+            using: function(position) {
                 $(this).css(position);
-                $("<div>")
-                    .addClass("arrow")
-                    .addClass(feedback.vertical)
-                    .addClass(feedback.horizontal)
-                    .appendTo(this);
+                $("<div>").addClass("arrow").appendTo(this);
             }
         }
     });
 
+    /**
+     * Tooltip block
+     * */
+    $('[data-tooltip-block]').tooltip({
+        position: {
+            my: 'left bottom-15',
+            at: 'left top',
+            using: function(position) {
+                $(this).css(position);
+                $("<div>").addClass("arrow").appendTo(this);
+            }
+        },
+        content:
+        '<div class="tooltip_col_1">' +
+        '<div>Transaction ID:</div>' +
+        '<div>Date:</div>' +
+        '<div>User:</div>' +
+        '<div>Order ID:</div>' +
+        '<div>Authorization Code:</div>' +
+        '<div>Avs Response:</div>' +
+        '<div>Cvv Response:</div>' +
+        '<div>Cavv Response:</div>' +
+        '</div>' +
+        '<div class="tooltip_col_2">' +
+        '<div>05daeeab-23a0-487c-a4a5-7e1b9ad7d8e8</div>' +
+        '<div>06/22/2015 12:30:54 PM</div>' +
+        '<div>Server$</div>' +
+        '<div>635705730407243751</div>' +
+        '<div>X6TYWJ</div>' +
+        '<div>Address and zip five match - Y</div>' +
+        '<div>Not processed - P</div>' +
+        '<div>Passed - 2</div>' +
+        '</div>'
+    });
 
 
 
